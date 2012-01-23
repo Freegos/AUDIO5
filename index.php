4fa5f7071a8542f380034634f3f0660b22ba2897
Adrien Albaladejo <adrien.albaladejo@gmail.com>
2012-01-23 20:54:06 +0100

    Ajout de l'alternative DewPlayer pour une meilleur compatibilit

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php 
 	class PlayerAudio5 {
 		
-			public function player($url_video_mp3, $url_video_ogg = NULL, $autoplay = FALSE, $preload = TRUE) {
-			echo $url_video_mp3;
-			echo $url_video_ogg;
-
+			public function player($url_audio_mp3, $url_audio_ogg = NULL, $autoplay = FALSE, $preload = TRUE) {
 			echo'
 			<audio controls preload>
-   				<source src="'.$url_video_mp3.'"></source>
-		 		<source src="'.$url_video_ogg.'"></source>
+   				<source src="'.$url_audio_mp3.'"></source>
+		 		<source src="'.$url_audio_ogg.'"></source>
+		 		
+		 		<!-- Version pour les navigateurs ne prenant pas en compte <audio></audio> -->
+		 		<object type="application/x-shockwave-flash" data="dewplayer/dewplayer.swf" width="200" height="20" id="dewplayer" name="dewplayer"> <param name="wmode" value="transparent" /><param name="movie" value="dewplayer.swf" /> <param name="flashvars" value="mp3='.$url_audio_mp3.'&amp;showtime=1" /> 
+		 		</object>
 			</audio>';
 
 		}
