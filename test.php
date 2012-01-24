053a3abe2dcc8e03591a1476d9d420746d301b5d
Adrien Albaladejo <adrien.albaladejo@gmail.com>
2012-01-24 09:08:02 +0100

    Envoie du fichier de test (PHP)

---
+++ test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>AUDIO5</title>
+</head>
+<body>
+	<?php 
+		require("index.php");
+
+		$url_audio_mp3 = "test.mp3";
+		$url_audio_ogg = "test.ogg";
+
+		$player = new PlayerAudio5;
+		$player->player($url_audio_mp3, NULL, FALSE, TRUE);
+		
+	
+	 ?>
+
+</body>
+</html>
